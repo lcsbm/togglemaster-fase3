7b0e40925a9bffd2b6cb917546bb14d4cb5a0b3c
lcsbm <lucas.sbm25@gmail.com>
2026-08-26 00:21:44 -0300

    fix: update S3 backend bucket name

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "togglemaster-tfstate"
+    bucket         = "togglemaster-tfstate-aae8e850"
     key            = "fase3/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
